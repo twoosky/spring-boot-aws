f7a00327c05f9bce0619f5846504a09fed827a35
twoosky <lhjae3@naver.com>
2022-01-14 22:29:09 +0900

    5.3 구글 로그인 연동하기

--- src/main/java/com/haneul/study/springboot/domain/user/User.java
+++ src/main/java/com/haneul/study/springboot/domain/user/User.java
@@ -34,7 +34,7 @@ public class User extends BaseTimeEntity {
     private Role role;
 
     @Builder
-    public void User(String name, String email, String picture, Role role){
+    public User(String name, String email, String picture, Role role){
         this.name = name;
         this.email = email;
         this.picture = picture;
